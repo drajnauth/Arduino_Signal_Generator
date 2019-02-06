ffdfe448641e95e2df2e642ef6a816a84a68e2a9
drajnauth <drajnauth@rogers.com>
2019-02-06 14:52:49 -0500

    Update

---
+++ PARC_Si5351_Signal_Generator_A_v0.1b/UART.h
@@ -0,0 +1,17 @@
+#ifndef _UART_H_
+#define _UART_H_
+
+
+#define RBUFF 40		// Max RS232 Buffer Size
+#define MAXIMUM_DIGITS 6	// Max numerical digits to process
+#define MAX_COMMAND_ENTRIES 6 
+
+
+
+
+char ProcessSerial ( void );
+unsigned char ParseSerial ( char *str );
+void ResetSerial (void);
+void ErrorOut ( void );
+
+#endif // _UART_H_
